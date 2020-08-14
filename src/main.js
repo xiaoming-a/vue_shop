2d096e1558bd19c53f184b5c7e3eae53195a6396
xiaomingzai <1421696906@qq.com>
2020-08-14 15:32:46 +0800

    完成商品分类功能

--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 
+
 // 导入全局样式表
 import './assets/css/global.css'
 // 导入字体图标
 import './assets/fonts/iconfont.css'
 //导入axios
 import axios from 'axios'
+
+import TreeTable from 'vue-table-with-tree-grid'
 
 //配置请求的根路径
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
@@ -21,6 +24,8 @@ Vue.prototype.$http = axios
 
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
