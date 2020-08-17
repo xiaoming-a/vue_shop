da76326fd3b10f7c1b575fd7b5e7e750e11a2b6f
xiaomingzai <1421696906@qq.com>
2020-08-17 08:05:04 +0800

    完成订单功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -37,6 +37,8 @@ import {
   CheckboxGroup,
   Checkbox,
   Upload,
+  Timeline,
+  TimelineItem
 } from 'element-ui'
 
 
@@ -77,6 +79,8 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
